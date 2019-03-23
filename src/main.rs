ef5be11c237e920281061fc7aac9633c963cebd3
Ferenc Nagy <nagy.ferenc.jr@protonmail.com>
2019-03-23 10:30:19 +0100

    Added editorconfig file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate regex;
 
 use crate::engine::board::Board;
 use crate::engine::board::chessmove::ChessMove;
-use crate::engine::board::square::{Rank, File, Square};
+use crate::engine::board::square::{File, Rank, Square};
 
 pub mod engine;
 
@@ -10,23 +10,23 @@ fn main() {
     let board = Board::new().make_move(
         ChessMove::new(
             Square::from_pos(Rank::B, File::Second),
-            Square::from_pos(Rank::D, File::Second)
+            Square::from_pos(Rank::D, File::Second),
         )).unwrap().make_move(
         ChessMove::new(
             Square::from_pos(Rank::G, File::Second),
-            Square::from_pos(Rank::E, File::Second)
+            Square::from_pos(Rank::E, File::Second),
         )).unwrap().make_move(
         ChessMove::new(
             Square::from_pos(Rank::A, File::Second),
-            Square::from_pos(Rank::C, File::Third)
+            Square::from_pos(Rank::C, File::Third),
         )).unwrap().make_move(
         ChessMove::new(
             Square::from_pos(Rank::G, File::Fourth),
-            Square::from_pos(Rank::E, File::Fourth)
+            Square::from_pos(Rank::E, File::Fourth),
         )).unwrap().make_move(
         ChessMove::new(
             Square::from_pos(Rank::C, File::Third),
-            Square::from_pos(Rank::E, File::Second)
+            Square::from_pos(Rank::E, File::Second),
         )).unwrap();
 
     println!("{}", board);
